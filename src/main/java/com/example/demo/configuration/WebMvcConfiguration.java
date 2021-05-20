63e470a1050c30c2c77f98eb0405c8a5ce0e44c9
권용빈 <yiyb0603@gmail.com>
2021-05-20 15:37:39 +0900

    Add: cors mapping

--- src/main/java/com/example/demo/configuration/WebMvcConfiguration.java
+++ src/main/java/com/example/demo/configuration/WebMvcConfiguration.java
@@ -4,6 +4,7 @@ import com.example.demo.interceptor.JwtInterceptor;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -15,5 +16,10 @@ public class WebMvcConfiguration implements WebMvcConfigurer {
   @Override
   public void addInterceptors(InterceptorRegistry registry) {
     registry.addInterceptor(jwtInterceptor).addPathPatterns("/post").excludePathPatterns("/auth/**");
+  }
+
+  @Override
+  public void addCorsMappings(CorsRegistry registry) {
+    registry.addMapping("/**").allowedOrigins("*").allowedMethods("*");
   }
 }
